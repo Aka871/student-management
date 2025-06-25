5c06f5674af29cbf7c4252977aa61be77ed18c15
Aka <163530203+Aka871@users.noreply.github.com>
2025-06-26 00:57:02 +0900

    feat: バリデーションエラー発生時の例外処理を追加 -MethodArgumentNotValidExceptionをハンドリングする処理を追加 -エラー発生時、400 Bad Requestと入力項目別のエラーメッセージを表示
    目的:入力エラーが発生したときに、適切なメッセージを表示することによって、問題の特定を容易にするため。

--- src/main/java/raisetech/studentmanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/raisetech/studentmanagement/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +15,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(
+      MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getFieldErrors().forEach(error -> {
+      errors.put(error.getField(), error.getDefaultMessage());
+    });
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
 
   /**
    * 受講生IDが見つからない例外をハンドリングします。
