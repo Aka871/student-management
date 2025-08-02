35821a21c1f859b4382c6ff7844d39272018ce05
Aka <163530203+Aka871@users.noreply.github.com>
2025-08-03 05:02:23 +0900

    feat: コース名が見つからない場合の例外処理を実装
    - Enum クラスで定義されていないコース名が指定された場合、CourseNotFoundException をスローする処理を追加
    - 該当の例外が発生した際、GlobalExceptionHandler で 404 Not Found ステータスと詳細メッセージを返すよう対応
    - CourseType というEnum クラスから、UNKNOWN (不明なコース) 定数を削除
    
    目的: 登録できるコース名を限定することで、DB の一貫性と整合性を保つため。

--- src/main/java/raisetech/studentmanagement/exception/CourseNotFoundException.java
+++ src/main/java/raisetech/studentmanagement/exception/CourseNotFoundException.java
@@ -1,8 +1,11 @@
 package raisetech.studentmanagement.exception;
 
+/**
+ * 入力されたコース名が見つからなかった場合にスローされる、例外クラスです。
+ */
 public class CourseNotFoundException extends RuntimeException {
 
-  public CourseNotFoundException(String message) {
-    super(message);
+  public CourseNotFoundException(String courseName) {
+    super("コース名: " + courseName + " が見つかりません");
   }
 }
